2a2d7beb78c1a625802521cb3df5f69556ffcfbb
saikumar14-08 <saikumar9808@gmail.com>
2025-03-14 10:58:45 -0500

    DaisyUI setup with theme and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import './App.css'
+import NavBar from "./NavBar";
 
 function App() {
   return (
     <>
-      <h1>Dev Connect Project</h1>
+      <NavBar />
+      <h1 className="text-3xl bg-red-500 font-bold underline">
+        Dev Connect Project
+      </h1>
+      <button class="btn btn-primary">Button</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
