2ee27584cac7d5d14a78430d690b3d2407991beb
saikumar14-08 <saikumar9808@gmail.com>
2025-04-02 23:50:42 -0500

    $ to rupee symbol change

--- src/components/PremiumPage.jsx
+++ src/components/PremiumPage.jsx
@@ -36,7 +36,7 @@ const PremiumPage = () => {
         <div className="card-body">
           <div className="flex justify-between">
             <h2 className="text-3xl font-bold">Plus</h2>
-            <span className="text-xl">$19/mo</span>
+            <span className="text-xl">₹ 19/mo</span>
           </div>
           <ul className="mt-6 flex flex-col gap-2 text-xs">
             <li>
@@ -106,7 +106,7 @@ const PremiumPage = () => {
           <span className="badge badge-xs badge-warning">Most Popular</span>
           <div className="flex justify-between">
             <h2 className="text-3xl font-bold">Premium</h2>
-            <span className="text-xl">$29/mo</span>
+            <span className="text-xl">₹ 29/mo</span>
           </div>
           <ul className="mt-6 flex flex-col gap-2 text-xs">
             <li>
