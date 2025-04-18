d3660693243a6247e8e0e5946f6372b6d252a17b
saikumar14-08 <saikumar9808@gmail.com>
2025-04-18 03:50:52 -0500

    Chat fix -1

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -5,7 +5,7 @@ export const createSocket = () => {
   if (location.hostname === "localhost") {
     return io(BASE_URL, { transports: ["websocket"], withCredentials: true });
   } else {
-    return io("/", {
+    return io("https://www.devconnekt.com", {
       path: "/api/socket.io",
       transports: ["websocket"],
       withCredentials: true,
